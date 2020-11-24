5e0e3ae6a4c27d9fac061aaa5065493695eaf09b
mamadou sylla <m27sylla@gmail.com>
2020-11-24 23:22:36 +0100

    création useState App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import fire from "./fire";
+import "./App.css";
 
 const App = () => {
+  const [user, setUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [EmailError, setEmailError] = useState('');
+  const [PasswordError, setPasswordError] = useState('');
+  const [hasAccount, sethasAccount] = useState(false);
+
+  const handleLogin = () => {
+
+  }
   return (
     <div className="App">
       <h1>Hello World</h1>
